583b8f48e75b10c40bdfddb7a710bd6894fbb22a
Md Noor Hadi <mdnoorhadi@gmail.com>
2020-09-21 06:47:03 +0800

    Got the timer js to work

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,32 @@
-var quizQuestions = [
-    "This is question 1.",
-    "This is question 2.",
-    "This is question 3."
-]
 
-console.log(quizQuestions);
+
+// SHOW TIMER
+
+function startTimer(duration, display) {
+    var getTimer = duration, minutes, seconds;
+    var setTimer = setInterval(function () {
+        minutes = parseInt(getTimer / 60, 10);
+        seconds = parseInt(getTimer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+        console.log(display.textContent);
+
+        if (--getTimer < 0) {
+            getTimer = duration;
+            clearInterval(setTimer); // stop the timer
+            alert("Uh oh! Time is up!"); // alert user that time is up
+            window.open("./timeout.html", "_self"); // open a new window
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var oneMinute = 10 * 1;
+    display = document.querySelector('#qTimer');
+    startTimer(oneMinute, display);
+};
+
+earnPoints();
